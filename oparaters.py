46fc327f7f9074815f78f1db8a0261381a264e08
Psadiq <96371957+Psadiq@users.noreply.github.com>
2021-12-22 00:07:34 +0530

    Create oparaters.py

---
+++ oparaters.py
@@ -0,0 +1,23 @@
+x = 10
+y = 30
+if x>0 and y>0:
+ print('Both x and y are positive numbers')
+ 
+ x = 10
+ y = 30
+ if x>0 or y>0:
+  print('At least one of x and y is positive numbers')
+
+a = 10
+b = 10
+a == b
+True  
+  
+
+a = 10
+b = 10
+
+print(a == b)
+print(a !=b)
+print(a > b)
+print(a < b)
